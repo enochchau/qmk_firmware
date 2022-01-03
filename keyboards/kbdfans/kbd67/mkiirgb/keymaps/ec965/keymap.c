288be2b84273002d39bd863b56ada841b94377dd
Enoch Chau <enoch@gatsby.events>
2022-01-03 10:55:38 -0800

    add bootmagic key swap for macos

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/ec965/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/ec965/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[0] = LAYOUT_65_ansi_blocker(
-			KC_ESC,        KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,         KC_MINS,  KC_EQL,  KC_BSPC,   KC_GRV,
+			KC_GESC,        KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,         KC_MINS,  KC_EQL,  KC_BSPC,   KC_GRV,
 		    KC_TAB,        KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,         KC_LBRC,  KC_RBRC, KC_BSLASH, KC_HOME,
 			LT(1,KC_CAPS), KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,      KC_QUOT,           KC_ENT,    KC_END,
 		    KC_LSFT,       KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,      KC_RSFT,           KC_UP,     KC_DEL,
@@ -11,5 +11,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    _______,       RGB_TOG,  RGB_MOD, RGB_HUI,RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK,  KC_PAUS, _______,  _______,
 			_______,	   RGB_SPI,  RGB_SPD, _______,_______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,  _______,          _______,  _______,
 		    _______,       _______,  _______, _______,_______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,  _______,          KC_PGUP,  _______,
-		    _______,       _______,  _______,                  _______,                   _______,          _______,  KC_END,           KC_PGDN,  _______),
+		    _______,       LAG_NRM,  LAG_SWP,                  _______,                   _______,          _______,  KC_END,           KC_PGDN,  _______),
 };
