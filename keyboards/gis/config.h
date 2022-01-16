5d049aea14146c73c1369084a55753ed46d0f2eb
ec965 <enoch965@gmail.com>
2022-01-16 00:14:22 -0800

    create gis

---
+++ keyboards/gis/config.h
@@ -0,0 +1,46 @@
+/* Copyright 2021 Enoch Chau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x9650
+#define PRODUCT_ID      0x1023
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    github.com/ec965
+#define PRODUCT         gis
+#define DESCRIPTION     split-keyboard
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 6
+
+// wiring of each half
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
+
+/* communication between sides */
+#define USE_SERIAL
+// #define SERIAL_USE_MULTI_TRANSACTION
+#define SOFT_SERIAL_PIN D2
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
